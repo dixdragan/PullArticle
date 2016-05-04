214c3fcfdd8df6d5e0c272ab2f13ea95d5ba11d6
ivanpbl <aaa@bbb.com>
2016-05-04 18:16:05 +0200

    functional example

--- app/src/main/java/com/example/dragan/pullarticles/ArticleView.java
+++ app/src/main/java/com/example/dragan/pullarticles/ArticleView.java
@@ -1,7 +1,24 @@
 package com.example.dragan.pullarticles;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ArticleView extends AppCompatActivity {
 
@@ -9,5 +26,60 @@ public class ArticleView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_view);
+        Bundle bundle=getIntent().getExtras();
+        URL url = null;
+        try {
+            url = new URL(bundle.getString("URL"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        new Task().execute(url);
+    }
+    private class Task extends AsyncTask<URL,Void,String>{
+        @Override
+        protected String doInBackground(URL... params) {
+            URL url=params[0];
+            String text = "";
+            BufferedReader reader = null;
+            try {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while((line = reader.readLine()) != null)
+            {
+                sb.append(line + "\n");
+            }
+            text = sb.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return text;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+            JSONObject jsonObject=new JSONObject(s);
+            String title = jsonObject.getString("title");
+            String introtext = jsonObject.getString("introtext");
+            String createdToParse = jsonObject.getString("created");
+            SimpleDateFormat sdfGet=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date createdDate= sdfGet.parse(createdToParse);
+            SimpleDateFormat sdfDisplay=new SimpleDateFormat("dd.MM.yyyy");
+            String createdToDisplay = sdfDisplay.format(createdDate).toString();
+                String text=title + "\n" + introtext + "\n" + createdToDisplay;
+                Html.fromHtml(text);
+            TextView textView= (TextView) findViewById(R.id.textView);
+            textView.setText(Html.fromHtml(text));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/dragan/pullarticles/ListOfArticles.java
+++ app/src/main/java/com/example/dragan/pullarticles/ListOfArticles.java
@@ -1,7 +1,23 @@
 package com.example.dragan.pullarticles;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ListOfArticles extends AppCompatActivity {
 
@@ -9,5 +25,65 @@ public class ListOfArticles extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_articles);
+        URL url=null;
+        try {
+            url = new URL("http://android.ogosense.net/interns/ace/articles.php");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        new Task().execute(url);
+
+    }
+
+    private class Task extends AsyncTask<URL,Void,String>{
+        @Override
+        protected String doInBackground(URL... params) {
+            String text = "";
+            BufferedReader reader = null;
+            try {
+            URL url=params[0];
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while((line = reader.readLine()) != null)
+            {
+                sb.append(line + "\n");
+            }
+            text = sb.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return text;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+            JSONArray jsonArray=new JSONArray(s);
+            for (int i=0;i<jsonArray.length();i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                final String id = jsonObject.getString("id");
+                final String title = jsonObject.getString("title");
+                TextView textView=new TextView(ListOfArticles.this);
+                textView.setText(id + " " + title);
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent articleIntent = new Intent(ListOfArticles.this, ArticleView.class);
+                        articleIntent.putExtra("URL", "http://android.ogosense.net/interns/ace/article.php?id=" + id);
+                        startActivity(articleIntent);
+                    }
+                });
+                LinearLayout linearLayout= (LinearLayout) findViewById(R.id.linearLayout);
+                linearLayout.addView(textView);
+            }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
